c77cb22c2fb5fc504b9791f09e10053921f08907
Mohamed Elkhalfy <mmelkhalfy@gmail.com>
2026-04-15 23:46:13 +0200

    added cosmetic features

--- app.js
+++ app.js
@@ -163,6 +163,13 @@ if (savedTheme === 'light') {
     if (modeBtn) modeBtn.innerText = '☀️';
 }
 
+window.addEventListener('scroll', () => {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById("scroll-progress").style.width = scrolled + "%";
+});
+
 
 
 
